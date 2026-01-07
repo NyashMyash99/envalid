22ddb9f8ca512ba089fc5f8cef57e3269d5a1afb
Daniil Koshkin <admin@nyashmyash99.dev>
2026-01-07 18:56:58 +0300

    feat: add project base (#1)
    * feat: add IDE folders to .gitignore
    
    * feat: add go.mod
    
    * feat(parser): add string parser
    
    * feat(parser): add port parser
    
    * feat(parser): add int32 parser
    
    * feat(parser): add int64 parser
    
    * feat(parser): add float32 parser
    
    * feat(parser): add float64 parser
    
    * feat(parser): add bool parser
    
    * feat(common): add validation and config filling function
    
    * docs: add README

---
+++ envalid.go
@@ -0,0 +1,102 @@
+package envalid
+
+import (
+	"errors"
+	"fmt"
+	"reflect"
+	"strings"
+)
+
+// ValidatorSupplier describes a function that provides a validator for the Schema.
+type ValidatorSupplier func() (any, error)
+
+// Supplier adapts a parser into a ValidatorSupplier.
+func Supplier[T any](p Parser[T], v Variable[T]) ValidatorSupplier {
+	return func() (any, error) {
+		return Validate(p, v)
+	}
+}
+
+// Schema maps config field names to their validators.
+type Schema map[string]ValidatorSupplier
+
+// Load loads and validates environment variables, creating a config with type T using the provided Schema.
+func Load[T any](s Schema) (*T, error) {
+	var zero T
+	rCfgType := reflect.TypeOf(zero)
+
+	k := rCfgType.Kind()
+	if k != reflect.Struct {
+		return nil, fmt.Errorf("Global errors:\n\t%s: got %s, want struct", rCfgType.Name(), k)
+	}
+
+	rCfg := reflect.New(rCfgType).Elem()
+
+	var errs []error
+
+	for i := 0; i < rCfgType.NumField(); i++ {
+		rField := rCfgType.Field(i)
+		fieldName := rField.Name
+
+		v := s[fieldName]
+		if v == nil {
+			continue
+		}
+
+		val, err := v()
+		if err != nil {
+			errs = append(errs, err)
+			continue
+		}
+
+		rVal := reflect.ValueOf(val)
+		if !rVal.Type().AssignableTo(rField.Type) {
+			return nil, fmt.Errorf(
+				"Global errors:\n\t%s.%s: got %s, want %s",
+				rCfgType.Name(),
+				fieldName,
+				rVal.Type(),
+				rField.Type,
+			)
+		}
+
+		rCfg.Field(i).Set(rVal)
+	}
+
+	var missingErrs, invalidErrs []error
+
+	for _, err := range errs {
+		var vErr *ValidationError
+		if errors.As(err, &vErr) {
+			switch vErr.Code {
+			case ErrNoValue:
+				missingErrs = append(missingErrs, err)
+			case ErrInvalidValue:
+				invalidErrs = append(invalidErrs, err)
+			}
+		}
+	}
+
+	var errMsg []string
+
+	if len(missingErrs) > 0 {
+		errMsg = append(errMsg, "Missing environment variables:")
+		for _, err := range missingErrs {
+			errMsg = append(errMsg, "\t"+err.Error())
+		}
+	}
+
+	if len(invalidErrs) > 0 {
+		errMsg = append(errMsg, "Invalid environment variables:")
+		for _, err := range invalidErrs {
+			errMsg = append(errMsg, "\t"+err.Error())
+		}
+	}
+
+	if len(errMsg) > 0 {
+		return nil, errors.New(strings.Join(errMsg, "\n"))
+	}
+
+	r := rCfg.Addr().Interface().(*T)
+	return r, nil
+}
---
+++ parser.go
@@ -0,0 +1,144 @@
+package envalid
+
+import (
+	"errors"
+	"fmt"
+	"strconv"
+	"strings"
+)
+
+// Parser describes a generic function that parses a string into a value of type T.
+//
+// It returns an error if the value is invalid for the target type.
+type Parser[T any] func(string) (T, error)
+
+/// ParseStr
+
+func trim(s string) string {
+	return strings.TrimSpace(s)
+}
+
+// ParseStr parses string s as a string.
+func ParseStr(s string) (string, error) {
+	return trim(s), nil
+}
+
+// Ensures ParseStr implements Parser[string]
+var _ Parser[string] = ParseStr
+
+/// ParseNumber
+
+func mapStrconvErr(err error, typ string) error {
+	if errors.Is(err, strconv.ErrSyntax) {
+		return fmt.Errorf("value must be a %s", typ)
+	}
+	if errors.Is(err, strconv.ErrRange) {
+		return fmt.Errorf("value must be in range of %s", typ)
+	}
+	return err
+}
+
+func parseIntGeneric(s string, bitSize int) (int64, error) {
+	v, err := strconv.ParseInt(trim(s), 10, bitSize)
+	if err != nil {
+		return 0, mapStrconvErr(err, fmt.Sprintf("int%d", bitSize))
+	}
+	return v, nil
+}
+
+func parseFloatGeneric(s string, bitSize int) (float64, error) {
+	v, err := strconv.ParseFloat(trim(s), bitSize)
+	if err != nil {
+		return 0, mapStrconvErr(err, fmt.Sprintf("float%d", bitSize))
+	}
+	return v, nil
+}
+
+/// ParseInt32
+
+// ParseInt32 parses string s as an integer number.
+//
+// It returns an error if string s is not a number or out of range.
+func ParseInt32(s string) (int32, error) {
+	v, err := parseIntGeneric(s, 32)
+	return int32(v), err
+}
+
+var _ Parser[int32] = ParseInt32
+
+/// ParseInt64
+
+// ParseInt64 parses string s as an integer number.
+//
+// It returns an error if string s is not a number or out of range.
+func ParseInt64(s string) (int64, error) {
+	v, err := parseIntGeneric(s, 64)
+	return v, err
+}
+
+var _ Parser[int64] = ParseInt64
+
+/// ParseFloat32
+
+// ParseFloat32 parses string s as a float number.
+//
+// It returns an error if string s is not a number or out of range.
+func ParseFloat32(s string) (float32, error) {
+	v, err := parseFloatGeneric(s, 32)
+	return float32(v), err
+}
+
+var _ Parser[float32] = ParseFloat32
+
+/// ParseFloat64
+
+// ParseFloat64 parses string s as a float number.
+//
+// It returns an error if string s is not a number or out of range.
+func ParseFloat64(s string) (float64, error) {
+	v, err := parseFloatGeneric(s, 64)
+	return v, err
+}
+
+var _ Parser[float64] = ParseFloat64
+
+/// ParseBool
+
+// BoolMap maps bool-like strings to bool values.
+var BoolMap = map[string]bool{
+	"1": true, "true": true, "t": true, "yes": true, "y": true, "on": true,
+	"0": false, "false": false, "f": false, "no": false, "n": false, "off": false,
+}
+
+// ParseBool parses string s as a bool.
+//
+// It returns an error if string s is not a bool.
+func ParseBool(s string) (bool, error) {
+	s = strings.ToLower(trim(s))
+	if v, ok := BoolMap[s]; ok {
+		return v, nil
+	}
+	return false, errors.New("value must be a bool-like")
+}
+
+var _ Parser[bool] = ParseBool
+
+/// ParsePort
+
+const (
+	minPort = 1
+	maxPort = 65535
+)
+
+// ParsePort parses string s as a TCP/UDP port (1–65535).
+//
+// It returns an error if string s is not a number or out of range.
+func ParsePort(s string) (uint16, error) {
+	v, err := parseIntGeneric(s, 16)
+	if err != nil || v < minPort || v > maxPort {
+		return 0, fmt.Errorf("port must be a number between %d and %d", minPort, maxPort)
+	}
+	return uint16(v), nil
+}
+
+var _ Parser[uint16] = ParsePort
