3b80ea141e2a40399b5cfc7dcdafb83c3d82035b
Daniil Koshkin <admin@nyashmyash99.dev>
2026-01-07 23:34:20 +0300

    test: add tests for ParseStr

---
+++ envalid_test.go
@@ -0,0 +1,33 @@
+package envalid
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestParseStr(t *testing.T) {
+	tests := []struct {
+		name     string
+		input    string
+		expected string
+	}{
+		{"should return string", "string", "string"},
+		{"should return string when input contains one letter", "s", "s"},
+		{"should return empty string when input is empty", "", ""},
+		{"should return empty string when input is whitespace", " ", ""},
+		{"should trim whitespace at beginning", " s", "s"},
+		{"should trim whitespace at ending", "s ", "s"},
+		{"should trim whitespaces", " s ", "s"},
+		{"should trim tabs and newlines", "\ts\n", "s"},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			res, err := ParseStr(tt.input)
+
+			assert.Equal(t, tt.expected, res)
+			assert.NoError(t, err)
+		})
+	}
+}
