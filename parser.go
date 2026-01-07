5efb6e37ab896cf08aa0030d2ada83e69b0cf8eb
Daniil Koshkin <admin@nyashmyash99.dev>
2026-01-07 18:33:38 +0300

    feat(parser)!: completely change ParsePort
    Remove empty value check.
    
    Remove use of ErrInvalid in favor of standard error due to disuse.
    
    Change result type from int to uint16.
    
    Add trim function for greater stability.
    
    Add mapStrconvErr function for error uniformity.
    
    Add parseIntGeneric function for subsequent implementation of number parsers.

--- parser.go
+++ parser.go
@@ -2,7 +2,9 @@ package envalid
 
 import (
 	"errors"
+	"fmt"
 	"strconv"
+	"strings"
 )
 
 // ErrEmpty indicates that a value is empty.
@@ -21,6 +23,10 @@ type Parser[T any] func(string) (T, error)
 // ParseStr parses string s as a non-empty string.
 //
 // Returns ErrEmpty if string s is empty.
+func trim(s string) string {
+	return strings.TrimSpace(s)
+}
+
 func ParseStr(s string) (string, error) {
 	if s == "" {
 		return "", ErrEmpty
@@ -32,26 +38,40 @@ func ParseStr(s string) (string, error) {
 var _ Parser[string] = ParseStr
 
 // ParsePort
+/// ParseNumber
+
+func mapStrconvErr(err error, typ string) error {
+	if errors.Is(err, strconv.ErrSyntax) {
+		return fmt.Errorf("value must be a %s", typ)
+	}
+	if errors.Is(err, strconv.ErrRange) {
+		return fmt.Errorf("value must be in range of %s", typ)
+	}
+	return err
+}
+
+func parseIntGeneric(s string, bitSize int) (int64, error) {
+	v, err := strconv.ParseInt(trim(s), 10, bitSize)
+	if err != nil {
+		return 0, mapStrconvErr(err, fmt.Sprintf("int%d", bitSize))
+	}
+	return v, nil
+}
 
 const (
 	minPort = 1
 	maxPort = 65535
 )
 
-// ParsePort parses string s as a port (1–65535).
+// ParsePort parses string s as a TCP/UDP port (1–65535).
 //
-// Returns ErrEmpty if string s is empty, ErrInvalid if string s is not a number or out of range.
-func ParsePort(s string) (int, error) {
-	if s == "" {
-		return 0, ErrEmpty
+// It returns an error if string s is not a number or out of range.
+func ParsePort(s string) (uint16, error) {
+	v, err := parseIntGeneric(s, 16)
+	if err != nil || v < minPort || v > maxPort {
+		return 0, fmt.Errorf("port must be a number between %d and %d", minPort, maxPort)
 	}
-
-	i, err := strconv.Atoi(s)
-	if err != nil || i < minPort || i > maxPort {
-		return 0, ErrInvalid
-	}
-
-	return i, nil
+	return uint16(v), nil
 }
 
-var _ Parser[int] = ParsePort
+var _ Parser[uint16] = ParsePort
